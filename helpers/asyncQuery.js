1893b1fd3e252b6a347a410174d90bbc8623ae32
James Q Quick <james.q.quick@gmail.com>
2021-12-20 15:57:29 -0600

    Update to use mysql2

--- helpers/asyncQuery.js
+++ helpers/asyncQuery.js
@@ -1,5 +1,17 @@
-import mysql from 'mysql';
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+// const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+const connection = mysql.createConnection({
+	host : process.env.DB_HOST,
+	user : process.env.DB_USER,
+	database: process.env.DB_DATABASE,
+	password : process.env.DB_PASSWORD,
+	ssl : {
+		rejectUnauthorized: true
+	}
+});
 
 export default async (query, values) => {
 	return new Promise( (resolve, reject) => {
