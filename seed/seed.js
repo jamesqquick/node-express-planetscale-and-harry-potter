cad52bdadca5ba3b25381577b6025767ded9d7e8
James Q Quick <james.q.quick@gmail.com>
2022-01-18 12:00:12 -0600

    Config with connection string

--- seed/seed.js
+++ seed/seed.js
@@ -4,15 +4,7 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connection = await mysql.createConnection({
-	host : process.env.DB_HOST,
-	user : process.env.DB_USER,
-	database: process.env.DB_DATABASE,
-	password : process.env.DB_PASSWORD,
-	ssl : {
-		rejectUnauthorized: true
-	},
-});
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 const characterIndexToWandIndex  = {};
 
